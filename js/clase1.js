9a9f8f8617c387d07a517da9551601289f692a0c
Gabriela Patricia Rodriguez <gprodriguez33@gmail.com>
2021-11-22 11:25:36 -0300

    practicando lunes

--- js/clase1.js
+++ js/clase1.js
@@ -250,7 +250,7 @@ registrados.*/
 /*Solicitar al usuario la carga de
 nombres de forma consecutiva, 
 hasta que se ingrese “Voldemort”.
-Luego realizar un única salida por
+Luego realizar una única salida por
 alerta, con todos los nombres
 ingresados.*/
 
@@ -314,4 +314,140 @@ ingresado en cada ciclo:
 //     }
 // }
 // console.log(calculadora(10, "*", 5));
+
+//CLASE 4
+
+/*Codificar tres funciones:
+● Una función entrada(), la cual solicite un valor al
+usuario y lo retorne.
+● Una función procesamiento(valor), donde se
+transforme la entrada.
+● Una función salida(valor), la cual mostrará el
+resultado por alerta.
+Luego, invocar las tres funciones.*/
+
+// function entrada(){
+//     return prompt("INGRESAR DATO");
+//    }
+// function procesamiento(valor){
+//     return "LA ENTRADA ES "+valor
+//    }
+// function salida(valor){
+//     alert(valor);
+//    }
+// //SUMAR LA OTRA FORMA DE LLAMADA
+// salida(procesamiento(entrada()));
+
+/*Codificar una función que reciba un número
+decimal por parámetro, y lo retorne redondeado
+al entero más cercano. Para ello, puede
+emplearse Math.round.
+Luego invocar la función de forma iterativa cinco
+(5) veces, solicitando en cada ciclo un número al
+usuario.*/
+
+// function redondeo(valor){
+//     return Math.round(valor)
+//    }
+
+// for (let i = 0; i < 5; i++) {
+//     let entrada = prompt("INGRESAR NUMERO");
+//     alert(redondeo(entrada));
+// }
+
+/*Codificar una función con la siguiente cabecera:
+impuesto(precio, porcentaje). En ella, se retorna
+un valor numérico compuesto por el precio
+recibido, más la suma del porcentaje ingresado.
+Luego invocar la función de forma iterativa cinco
+(5) veces, solicitando en cada ciclo un precio y
+porcentaje al usuario.*/
+
+// function impuesto (precio, porcentaje) {
+//  let impuesto = ((precio * porcentaje)/100);
+//     return precio + impuesto;
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     let entradaPrecio = Number(prompt("ingrese el precio"));
+//     let entradaImpuesto = Number(prompt("ingrese el porcentaje"));
+//     alert(impuesto(entradaPrecio, entradaImpuesto));
+// }
+
+//Otra forma
+// function impuesto(precio, porcentaje){
+//     return precio + ((precio * porcentaje)/100)
+//    }
+//    for (let index = 0; index < 5; index++) {
+//     let resultado = impuesto(parseFloat(prompt("INGRESAR PRECIO"))
+//     ,parseFloat(prompt("INGRESAR %")));
+//     alert(resultado);
+//    }
+   
+/*Codificar dos funciones:
+● Una función cotizarDolar(pesos), la cual recibe un
+valor monetario en pesos argentinos, y lo retorna
+en su equivalente en dólares.
+● Una función cotizarPesos(dolar), que recibe un
+valor monetario en dólares, y lo retorna en su
+equivalente en pesos argentinos.
+Luego invocar las funciones solicitando al usuario el
+valor y el tipo de cotización a realizar.*/
+
+// function cotizarDolar(pesos) {
+//     let dolarOficial = 100.45;
+//     return pesos / dolarOficial;
+// }
+// console.log(cotizarDolar(500))
+
+// function cotizarPesos(dolar) {
+//     let pesoOficial = 0.0099;
+//     return dolar / pesoOficial;
+// }
+
+// console.log(cotizarPesos(5))
+
+//Otra forma
+
+// const COTIZACION_DOLAR = 100.45;
+// const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
+// const cotizarPesos = (dolar) => dolar * COTIZACION_DOLAR;
+// let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - DOLARES A PESOS \n 2 - PESOS A DOLAR ");
+// let valor = prompt("VALOR");
+// switch (seleccion) {
+//  case "1":
+//  alert(cotizarPesos(valor));
+//  break;
+//  case "2":
+//  alert(cotizarDolar(valor));
+//  break;
+//  default:
+//  break;
+// }
+
+/*Codificar una función con la siguiente cabecera:
+validacion(cadena). En ella, se retorna un valor
+booleano, el cual es true si el parámetro no es
+un cadena vacía. Caso contrario, se retorna
+false. Luego invocar la función de forma iterativa,
+hasta que el usuario tipee “ESC”, solicitando en
+cada ciclo una cadena a validar.*/
+
+// function validacion(cadena) {
+//     if (cadena !== "") {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// let entrada = prompt("ingrese una cadena"); 
+// while (entrada !== "ESC") {
+//     alert(validacion(entrada));
+//     entrada = prompt("ingrese una cadena");
+// } 
+
+
+
+
 
